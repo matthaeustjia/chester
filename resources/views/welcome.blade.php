aff73a7e69fba55e8fcb88d66b6d5ae9f52fd439
= <=>
2018-06-05 03:32:08 +1000

    Index page is done

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Time5lip</title>
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-top">
+                <h1>ChesterChan</h1>
+            </div>
+                <nav>
+                    <a href="/home">Home</a>
+                    <a href="/gallery">Gallery</a>
+                    <a href="/about">About</a>
+                    <a href="/contact">Contact</a>
+                </nav>
+        </div>
+    </header>
+</body>
+</html>
